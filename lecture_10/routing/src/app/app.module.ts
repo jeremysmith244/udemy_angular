0de9af916d8a3cc6f3c84b2e860920561c726760
Jeremy Smith <jeremysmith244@gmail.com>
2021-08-21 20:48:00 -0700

    started lecture 10

---
+++ lecture_10/routing/src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GithubServiceService } from './services/github-service.service';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { FollowerComponent } from './follower/follower.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PostComponent } from './posts/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FollowerComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    GithubProfileComponent,
+    HomeComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      // Routes are called in order!!!
+      { 
+        path: '', 
+        component: HomeComponent
+      },
+      { 
+        path: 'followers/follower/:username', 
+        component: GithubProfileComponent
+      },
+      { 
+        path: 'followers', 
+        component: FollowerComponent
+      },
+      {
+        path: 'posts',
+        component: PostComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ])
+  ],
+  providers: [
+    GithubServiceService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lecture_10/routing/src/app/services/data-service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { AppError } from '../errors/app-error';
+import { NotFoundError } from '../errors/not-found-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { throwError } from 'rxjs';
+
+export class DataService {
+  constructor(private url: string, private http: HttpClient) {
+    // Constructor in angular should only be used to manage dependency injections
+  }
+
+  getAll() {
+    return this.http.get(this.url)
+      .pipe(catchError(err => {
+        return throwError( this.handleError(err) );
+      }));
+  }
+
+  create(resource: any) {
+    return this.http.post(this.url, JSON.stringify(resource))
+      .pipe(catchError(err => {
+        return throwError( this.handleError(err) );
+      }));
+  }
+
+  getItemUrl(resource: any) {
+    return this.url + '/' + resource.id;
+  }
+
+  update(resource: any) {
+    this.http.put(this.getItemUrl(resource), JSON.stringify(resource))
+      .pipe(catchError(err => {
+        return throwError( this.handleError(err) );
+      }));
+  }
+
+  delete(resource: any) {
+    return this.http.delete(this.getItemUrl(resource))
+      .pipe(catchError(err => {
+        return throwError( this.handleError(err) );
+      }))
+  }
+
+  private handleError(err: Response) {
+  if (err.status === 400)
+    return new BadRequestError(err);
+  if (err.status === 404)
+    return new NotFoundError(err);
+  else
+    return new AppError(err);
+  }
+
+}
